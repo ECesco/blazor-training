4f0c66eeaa23875b093926125e895807bacee3e8
Enzo Cesco <enzo.cesco@gmail.com>
2022-04-21 23:25:17 -0300

    first commit

---
+++ Shared/BlazorWeb.Domain/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace BlazorWeb.Domain
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public bool IsActive { get; set; }
+        public double Price { get; set; }
+
+    }
+}
